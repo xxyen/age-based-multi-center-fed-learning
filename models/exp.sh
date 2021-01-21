e2066819f9e598a5e35b7fb69354ab475ee07b91
David Xie <ming.xie-1@student.uts.edu.au>
2021-01-21 20:55:45 +1100

    make sure everything is in the svn control, now I can rm any local copy

--- models/exp.sh
+++ models/exp.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-python3 experiment.py -dataset femnist -experiment fedsem -configuration four.yaml
-
-python3 experiment.py -dataset femnist -experiment fedsem -configuration two.yaml
+python3 experiment.py -dataset femnist -experiment fedavg
+python3 experiment.py -dataset celeba -experiment fedavg
--- models/femnist/show_cels.py
+++ models/femnist/show_cels.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.gridspec as gridspec
 
-
 import os
 import numpy as np
 import random
 from PIL import Image
 
 root_path = os.path.dirname(os.path.dirname(os.path.abspath('')))
-parent_path = os.path.join(root_path, "data", "celeba")
+## change this to your folder of raw of celeba faces
+parent_path = os.path.join(root_path, "..",  "..", "multi-center-fed-learning","data", "celeba")
 rawdata_path = os.path.join(parent_path, 'data', 'raw', 'img_align_celeba')
 
 def get_metadata():
@@ -42,27 +42,42 @@ cels = get_celebrities_and_images(get_metadata()[0])
         
 
 def show_by_cel(j):
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(12, 12))
     p = cels[j]
-    if len(p) > 12:
-        sub_cel = p[:12]
+    tot_imgs = 4 * 4
+    if len(p) > tot_imgs:
+        sub_cel = p[:tot_imgs]
     else:
         sub_cel = p
 
     i = 0
-#     size = 28, 28, 3  # original image size is 84, 84
     for f in sub_cel:
         file_path = os.path.join(rawdata_path, f)
         img = Image.open(file_path)
-#         rgb = img.thumbnail(size)
         rgb = np.array(img).copy()
-        plt.subplot(3, 4, i + 1)
+        plt.subplot(4, 4, i + 1)
         plt.xticks([])
         plt.yticks([])
-        plt.grid(False)
+        plt.grid(True)
         plt.imshow(rgb /255.0)
-        i += 1       
+        i += 1
 
     plt.show()
-
     
+    return sub_cel
+
+def show_by_file(files):
+    plt.figure(figsize=(12, 12))
+    
+    i = 0
+    for f in files:
+        file_path = os.path.join(rawdata_path, f)
+        img = Image.open(file_path)
+        rgb = np.array(img).copy()
+        plt.subplot(4, 4, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(rgb /255.0)
+        i += 1
+
+    plt.show()    
